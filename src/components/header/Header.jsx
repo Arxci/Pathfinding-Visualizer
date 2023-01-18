b30af7b3ca695ecf9416e2287167d398f3666823
Arxci <garretthumbert9@gmail.com>
2023-01-17 20:24:22 -0600

    Please enter the commit message for your changes. Lines starting

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header className="header">
+			<nav className="header__nav container">
+				<ul className="header__nav__list">
+					<li className="header__nav__item title">
+						<h1>XTREME</h1>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
+
+export default Header
